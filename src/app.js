79731cc4cfb7d3796d7c60892f8d43a3bf24731b
archaic10 <vitor.franca.9400@gmail.com>
2021-04-22 10:37:29 -0300

    Installing ts-node-dev globally with development dependency

--- src/app.js
+++ src/app.js
@@ -4,10 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var cors_1 = __importDefault(require("cors"));
 var routes_1 = require("./routes");
 var app = express_1.default();
 exports.app = app;
-app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(routes_1.router);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express from 'express';
-import cors from 'cors';
+
 import { router } from './routes';
 
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 app.use(router);
